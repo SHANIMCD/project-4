1462d4d95447dcb1784519b9ddbfa92fdb2e846e
Shani McDonald <shanilmcdonald@gmail.com>
2019-11-13 19:26:26 +0000

    commit

--- project/settings.py
+++ project/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default':{
+'ENGINE':'django.db.backends.postgresql_psycopg2',
+'NAME':'django-gyms',
+'HOST':'localhost',
+'PORT':5432
+}
 }
 
 
